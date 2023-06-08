7eb8b36f39f3d27cda846cec4fcd5ae5fc923294
Aaron Spaulding <15004857+acs14007@users.noreply.github.com>
2023-06-08 15:03:29 -0400

    Version 1.3.4

--- download_hrrr.py
+++ download_hrrr.py
@@ -1,26 +1,50 @@
-import os
-from datetime import datetime, timedelta
-from herbie import Herbie, Herbie_latest, FastHerbie
-
-
 # https://mesowest.utah.edu/html/hrrr/zarr_documentation/html/zarr_variables.html
-base_path = os.path.join(os.path.join(os.getcwd(), 'weather_data'), 'hrrr')
 
-def get_latest_hrrr_time():
-    current_time = datetime.utcnow()
+import os
+import json
+from datetime import datetime, timedelta
+from herbie import Herbie
 
-    if current_time.hour < 6:
-        forecast_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
-    elif current_time.hour < 12:
-        forecast_time = current_time.replace(hour=6, minute=0, second=0, microsecond=0)
-    elif current_time.hour < 18:
-        forecast_time = current_time.replace(hour=12, minute=0, second=0, microsecond=0)
+base_path = os.getcwd()
+weather_base_path = os.path.join(os.path.join(base_path, 'weather_data'), 'hrrr')
+current_time = datetime.utcnow()
+
+hrrr_timebuffer = timedelta(hours=1, minutes=30)
+
+status_file_path = os.path.join(base_path, 'status.json')
+if os.path.exists(status_file_path):
+    with open(status_file_path, 'r') as file:
+        status = json.load(file)
+else:
+    status = {
+        'hrrr_last_updated_time_utc': None,
+        'hrrr_valid_time_utc': None,
+    }
+
+
+def get_latest_hrrr_time(current_time_):
+    if current_time_.hour < 6:
+        forecast_time = current_time_.replace(hour=0, minute=0, second=0, microsecond=0)
+    elif current_time_.hour < 12:
+        forecast_time = current_time_.replace(hour=6, minute=0, second=0, microsecond=0)
+    elif current_time_.hour < 18:
+        forecast_time = current_time_.replace(hour=12, minute=0, second=0, microsecond=0)
     else:
-        forecast_time = current_time.replace(hour=18, minute=0, second=0, microsecond=0)
+        forecast_time = current_time_.replace(hour=18, minute=0, second=0, microsecond=0)
+    return forecast_time
 
-    return forecast_time.strftime('%Y-%m-%d %H:%M:%S')
 
-H = Herbie(get_latest_hrrr_time())
+# Update HRRR Data
+latest_hrrr_time = get_latest_hrrr_time(current_time - hrrr_timebuffer)
+H = Herbie(latest_hrrr_time.strftime('%Y-%m-%d %H:%M:%S'))
 near_surface_smoke_data = H.xarray('MASSDEN')
 near_surface_smoke_data = near_surface_smoke_data.mdens.to_pandas()
-near_surface_smoke_data.to_parquet(os.path.join(base_path, 'near_surface_smoke.parquet'))
+
+status['hrrr_last_updated_time_utc'] = current_time.strftime('%Y-%m-%d %H:%M:%S')
+status['hrrr_valid_time_utc'] = latest_hrrr_time.strftime('%Y-%m-%d %H:%M:%S')
+
+near_surface_smoke_data.to_parquet(os.path.join(weather_base_path, 'near_surface_smoke.parquet'))
+
+# Write out status
+with open(status_file_path, 'w') as file:
+    json.dump(status, file, indent=4)
